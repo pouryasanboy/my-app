6c405ce4ea95d515ae08834f11505fceef4e12f9
Pourya BehzadPour <pourya@atlann.co>
2022-08-24 13:36:38 +0430

    asdasd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">Something bad happened</div>
+    <div v-else>{{data}}</div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import { request } from "./datocms";
+const HOMEPAGE_QUERY = `query HomePage($limit: IntType) {
+  allBlogPosts(first: $limit) {
+    title
   }
-}
+}`;
+export default {
+  name: "App",
+  data: () => ({
+    data: null,
+    error: null,
+    loading: true,
+  }),
+  async mounted() {
+    try {
+      this.data = await request({
+        query: HOMEPAGE_QUERY,
+        variables: {
+          limit: 10
+        }
+      });
+    } catch (e) {
+      this.error = e;
+    }
+    this.loading = false;
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
